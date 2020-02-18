6d9ea2e1f6b35dfa6fbbc402af2a399788bcb0ba
Brian Vehige <brianvehige@gmail.com>
2020-02-18 06:54:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2 
     return array.join(" and ") 
   else array.length >= 3 
-    updated_last_item = (", and #{array[-1]}")
+    updated_last_item = ("and #{array[-1]}")
+    
     return array.join(", ")
   end
 end
