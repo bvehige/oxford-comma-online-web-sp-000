733c6a4bcb3c44d274a6265e30bd3fe798279360
Brian Vehige <brianvehige@gmail.com>
2020-02-18 06:20:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2 
     return array.join(" and ") 
-  elsif array.length >= 3 
-  
+  else array.length >= 3 
+    return array.join(", ")
+  end
 end
